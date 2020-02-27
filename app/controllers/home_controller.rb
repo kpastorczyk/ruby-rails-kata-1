a6bd5d916fabf5f765d9a3603a0e9974b483b23f
Krzysztof Pastorczyk <kpastorczyk@gmail.com>
2020-02-27 16:45:59 +0100

    Search query use magazine repository too

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,17 +6,16 @@ class HomeController < ApplicationController
 
   def search
     @items ||= find_by(params['isbn'])
-
     render 'index'
   end
 
   private
 
   def set_items
-    @items ||= Book.select(:isbn, :title, :authors).all + Magazine.select(:isbn, :title, :authors).all
+    @items ||= Book.all + Magazine.all
   end
 
   def find_by(isbn)
-    Book.where("isbn LIKE '%#{isbn}%'")
+    Book.where("isbn LIKE '%#{isbn}%'") + Magazine.where("isbn LIKE '%#{isbn}%'")
   end
 end
