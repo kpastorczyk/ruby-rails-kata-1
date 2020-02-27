34e1499c0218880dadb62b338e21eb10608c89b2
Krzysztof Pastorczyk <kpastorczyk@gmail.com>
2020-02-27 16:39:16 +0100

    User can find item by isbn

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,22 @@
 class HomeController < ApplicationController
-  before_action :set_items
+  before_action :set_items, only: [:index]
 
   def index
+  end
+
+  def search
+    @items ||= find_by(params['isbn'])
+
+    render 'index'
   end
 
   private
 
   def set_items
     @items ||= Book.select(:isbn, :title, :authors).all + Magazine.select(:isbn, :title, :authors).all
+  end
+
+  def find_by(isbn)
+    Book.where("isbn LIKE '%#{isbn}%'")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'home/index'
+  post '/', to: 'home#search'
   root 'home#index'
 end
